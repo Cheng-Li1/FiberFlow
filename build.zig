4d5822b3490c26241e987e8dcbcefd05131f3104
Cheng <lichengchaoreng@gmail.com>
2024-05-23 14:31:06 +1000

    Minor changes to the build.zig

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
+// This file is designed for Zig build system 0.12
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // It should be default to debug if not specified
     const optimize = b.standardOptimizeOption(.{});
 
     // Change the target platform here if needed
@@ -9,12 +11,14 @@ pub fn build(b: *std.Build) void {
         .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_a53 },
         .os_tag = .freestanding,
         .abi = .none,
+        .ofmt = .elf,
     });
 
     const lib = b.addStaticLibrary(.{
-        .name = "FiberFlow.a",
+        .name = "FiberFlow",
         .target = target,
         .optimize = optimize,
+        // For library, we should always maintain the Symbol Table
         .strip = false,
     });
 
